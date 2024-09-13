aa7a86afa79e4c4f0a192c0e00e9cf7b1e019933
Franklin Henderson <fhwrdh@fhwrdh.net>
2024-09-13 10:25:36 -0700

    email

--- src/About.jsx
+++ src/About.jsx
@@ -5,9 +5,9 @@ export default function About() {
     <Box display={{ base: "block", md: "flex" }}>
       <Box w={{ base: "full", md: "75%" }} mr={8}>
         <Text mb={4}>
-          I am a film photographer based near Seattle, Washington.
+          I am a film photographer based near Seattle, Washington, USA.
         </Text>
-        <Text mb={4}>photo@fhwrdh.net</Text>
+        <Link href="mailto:photo@fhwrdh.net">photo@fhwrdh.net</Link>
       </Box>
       <Image src="/fhwrdh.png" width="100%" maxWidth="300px" />
     </Box>
